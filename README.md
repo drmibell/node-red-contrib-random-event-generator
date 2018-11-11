1f2b0719d18518ac3d32c0d72c56a2bd836bc68b
Michael I Bell <dr.m.i.bell@gmail.com>
2018-11-11 17:36:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# node-red-contrib-random-event-generator
+A Node-RED node for generating events at random intervals.
+## Install
+Either use the Manage Palette option in the Node-RED Editor menu, or run the following command in your Node-RED user directory (typically `~/.node-red`):
+
+    npm install node-red-contrib-random-event-generator
+## Usage
+This node generates events (messages) at intervals drawn from a random distribution. By default, an exponential distribution is used, where the probability of a given interval between events is an exponentially decreasing function of the length of the interval. This is the behaviour displayed by radioactive decays, noise in electronic devices, and other physical phenomena. Alternatively, the distribution can be chosen to be uniform in a selected range.
+
+The user can specify the average interval for the exponential distribution and the minimum and maximum intervals for the uniform distribution. Negative inputs are replaced by their absolute values.
+
+Messages with the topic <code>Control Topic</code> are control messages, which toggle 
+the generator on or off. The output messages have a user-defined topic (default: `event`) and payload (default: `timestamp`) and the additional property `msg.delay`, which is the time since the previous event.
+
+The node has been useful in testing the operation of message queues and in Monte-Carlo simulation of physical systems.
+# Node status
+The state of the node is indicated by a status object, a red dot when the generator is stopped and a green dot with the value of the current interval when it is running.
+## Examples
+This flow demonstrates the basic operation of the `event` node. The average interval between events is calculated with `node-red-contrib-statistics`. This can be omitted, but the user may be interested in seeing how quickly the average converges to its expected value.
+```
+[{"id":"24a72f57.5327e8","type":"inject","z":"6f10feeb.9097f8","name":"start/stop","topic":"control","payload":"","payloadType":"date","repeat":"","crontab":"","once":false,"onceDelay":0.1,"x":140,"y":80,"wires":[["371a8a7f.53a956"]]},{"id":"2cc4ce7e.b0853a","type":"debug","z":"6f10feeb.9097f8","name":"","active":false,"tosidebar":true,"console":false,"tostatus":false,"complete":"delay","x":430,"y":60,"wires":[]},{"id":"371a8a7f.53a956","type":"event","z":"6f10feeb.9097f8","name":"","controlTopic":"control","meanInterval":"1","distribution":"exponential","minInterval":"1","maxInterval":"2","outputTopic":"event","outputPayload":"timestamp","x":270,"y":80,"wires":[["2cc4ce7e.b0853a","4a1659df.b68d"]]},{"id":"bf964625.93036","type":"debug","z":"6f10feeb.9097f8","name":"","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"payload","x":730,"y":100,"wires":[]},{"id":"4a1659df.b68d","type":"change","z":"6f10feeb.9097f8","name":"mean","rules":[{"t":"set","p":"topic","pt":"msg","to":"data/mean","tot":"str"}],"action":"","property":"","from":"","to":"","reg":false,"x":410,"y":100,"wires":[["124accec.44cf1b"]]},{"id":"124accec.44cf1b","type":"statistics","z":"6f10feeb.9097f8","name":"mean delay","dataSetSize":0,"inputField":"delay","inputFieldType":"msg","resultField":"payload","resultFieldType":"msg","parameterField":"","parameterFieldType":"payload","stripFunction":false,"resultOnly":true,"x":570,"y":100,"wires":[["bf964625.93036"]]},{"id":"d9a3ebaf.c283b","type":"inject","z":"6f10feeb.9097f8","name":"clear","topic":"clear","payload":"","payloadType":"date","repeat":"","crontab":"","once":false,"onceDelay":0.1,"x":410,"y":140,"wires":[["124accec.44cf1b"]]}]
+```
+<img src="https://github.com/drmibell/node-red-contrib-queue-gate/blob/master/screenshots/q-gate-demo.png?raw=true"/>
+
---
+++ event.js
@@ -0,0 +1,121 @@
+/**
+ * Copyright 2017 M. I. Bell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+"use strict";
+module.exports = function(RED) {
+    function RandomEventNode(config) {
+        RED.nodes.createNode(this,config);
+        // Copy configuration items
+        this.controlTopic = config.controlTopic.toLowerCase();
+        this.distribution = config.distribution;
+        this.meanInterval = Math.abs(parseFloat(config.meanInterval)); //seconds
+        this.minInterval = Math.abs(parseFloat(config.minInterval));
+        this.maxInterval =  Math.abs(parseFloat(config.maxInterval));
+        this.outputPayload = config.outputPayload || 'timestamp';
+        this.outputTopic = config.outputTopic || 'event';
+        // Save "this" object
+        var node = this;
+        var distribution = node.distribution;
+//        node.meanInterval = parameterTest(node.meanInterval);
+//        node.maxInterval = parameterTest(node.maxInterval);
+//        node.minInterval = parameterTest(node.minInterval);
+        var mean = node.meanInterval || 1;
+        var min = node.minInterval || 1;
+        var max = node.maxInterval || 2;
+        var context = node.context();
+        var msgToSend = null;
+        var run = context.get('state') || false;
+//        node.warn(mean);
+//        node.warn(min);
+//        node.warn(max);
+        
+        node.on('input', function(msg) {
+            if (msg.topic.toLowerCase() === 'control') {
+                run = ! run;
+            } else {
+                return null;
+            }
+            context.set('state',run);
+            node.status({fill:(run) ? 'green':'red'});
+            context.set('output',msg);
+            loop();
+            })
+            
+        function loop() {
+            var delay = 0
+            msgToSend = context.get('output');
+            if (msgToSend.topic.toLowerCase() === 'control') {
+                msgToSend = null;
+            }
+                node.send(msgToSend);
+            if (context.get('state')) {
+                var msgToSend = context.get('output');
+//                msgToSend.distribution = distribution;
+//            msgToSend.distribution = (distribution = 'exponential') ? distribution +', '+ mean : distribution + min +'-'+ max;
+//                var distProperty = (distribution = 'exponential') ? distribution +', '+ mean : distribution + min +'-'+ max;
+//                msgToSend.distribution = distProperty;
+//            node.warn(distribution);
+                switch (distribution) {
+                    case 'exponential':
+                        delay = expInterval(mean);
+                        break;
+                    case 'uniform':
+                        delay = uniformInterval(min,max);
+                        break;
+                    default:
+                        node.error('No distribution specified');
+                        return
+                }
+//                node.warn(delay);
+//                node.warn(typeof delay);
+                msgToSend.payload = node.outputPayload;
+                
+                if (msgToSend.payload === 'timestamp') {
+                    msgToSend.payload = Date.parse (new Date());
+
+                }
+//                msgToSend.payload = Date.parse (new Date());
+                msgToSend.topic = node.outputTopic;
+                msgToSend.delay = delay;
+                context.set('output',msgToSend);
+                setTimeout(loop,1000 * delay);
+                node.status({fill:'green',text:delay.toFixed(2)});
+            }
+        }
+        
+        function expInterval(m) { // see Knuth, Vol. 2, p. 133
+            var u = Math.random();
+            if (u === 0){
+                u = 5e-234;
+            }
+            return - m * Math.log(u);
+        }
+
+        function uniformInterval(min,max) {
+            return Math.random() * (max - min) + min;
+        }
+        
+/*
+        function parameterTest(x) {
+            if (x < 0) {
+                node.warn('Negative parameter replaced with absolute value');
+                return -x
+            }
+        }
+*/            
+
+    }
+    RED.nodes.registerType("event",RandomEventNode);
+}
