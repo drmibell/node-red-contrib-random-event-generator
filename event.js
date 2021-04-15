3b1a6388ee34ccea71822479df626df6b89a888c
Michael I Bell <dr.m.i.bell@gmail.com>
2021-04-15 14:52:59 -0400

    add gaussian distribution, msg.count

--- event.js
+++ event.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017-2020 M. I. Bell
+ * Copyright 2017-2021 M. I. Bell
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,106 +16,153 @@
 "use strict";
 module.exports = function(RED) {
     function RandomEventNode(config) {
-        RED.nodes.createNode(this,config);
-        // Copy configuration items
-        this.controlTopic = config.controlTopic.toLowerCase();
-        this.startCmd = config.startCmd.toLowerCase();
-        this.stopCmd = config.stopCmd.toLowerCase();
-        this.distribution = config.distribution;
-        this.meanInterval = Math.abs(parseFloat(config.meanInterval)); //seconds
-        this.minInterval = Math.abs(parseFloat(config.minInterval));
-        this.maxInterval =  Math.abs(parseFloat(config.maxInterval));
-        this.outputPayload = config.outputPayload || 'timestamp';
-        this.outputTopic = config.outputTopic || 'event';
-        // Save "this" object
-        var node = this;
-        var distribution = node.distribution;
-        var mean = node.meanInterval || 1;
-        var min = node.minInterval || 1;
-        var max = node.maxInterval || 2;
-        var context = node.context();
-        var msgToSend = null;
-        var run = context.get('run') || false;
-        var run = context.get('state') || false;
-        var startTimer
-        
+        RED.nodes.createNode(this,config)
+        this.controlTopic = config.controlTopic.toLowerCase()
+        this.startCmd = config.startCmd.toLowerCase()
+        this.stopCmd = config.stopCmd.toLowerCase()
+        this.distribution = config.distribution
+        this.expMeanInterval =  Math.abs(parseFloat(config.expMeanInterval)) //seconds
+        this.minInterval =  Math.abs(parseFloat(config.minInterval))
+        this.maxInterval =   Math.abs(parseFloat(config.maxInterval))
+        this.gaussMeanInterval =  Math.abs(parseFloat(config.gaussMeanInterval))
+        this.stdDev =  Math.abs(parseFloat(config.stdDev))
+        this.maxEventCount = config.maxEventCount
+        this.outputPayload = config.outputPayload
+        this.outputPayloadType = config.outputPayloadType
+        this.outputTopic = config.outputTopic || 'event'
+        let node = this
+        let controlTopic = node.controlTopic
+        let distribution = node.distribution
+        let expMean = node.expMeanInterval || 1
+        let min = node.minInterval || 1
+        let max = node.maxInterval || 2
+        let gaussMean = node.gaussMeanInterval || 1
+        let stdDev = node.stdDev || 1
+        let context = node.context()
+        let outputTopic = node.outputTopic
+        let outputPayload = node.outputPayload
+        let outputPayloadType = node.outputPayloadType
+        let payload = RED.util.evaluateNodeProperty(outputPayload, outputPayloadType, node)
+        let msgToSend = {"count":-1}    // debug
+        let maxEventCount = node.maxEventCount
+        if (maxEventCount <= 0) {maxEventCount = Infinity}
+        let state = 'stopped'
+        context.set('state',state)
+        let stoppedStatus = {fill:'red',shape:'ring',text:'stopped'}    // debug
+        node.status(stoppedStatus)  // debug
+        let timer, delay
+
         node.on('input', function(msg) {
-            if (typeof msg.topic === 'undefined' || typeof msg.payload === 'undefined') {
-                return null;
-            }
-            if (msg.topic.toLowerCase() !== node.controlTopic) {
-                return null;
-            }
-            // Change state
+            if (typeof msg.topic !== 'string') {return}
+            if (msg.topic.toLowerCase() !== controlTopic) {return}
+            state = context.get('state')
             switch (msg.payload.toString().toLowerCase()) {
                 case node.startCmd:
-                    run = true;
-                    break;
+                    if (state === 'running') {  // already running
+                        node.warn('command ignored: already running')
+                        return                    
+                    } else {
+                        state = 'started'
+                    }
+                    break
                 case node.stopCmd:
-                    run = false;
-                    break;
-                default:
-                    run = ! run;
+                    if (state === 'stopped') { // already stopped
+                        node.warn('command ignored: already stopped')
+                        return
+                    }
+                    state = 'stopped'
+                    break
+                default:    // toggle
+                    if (state === 'stopped') {
+                        state = 'started'
+                    } else {
+                        state = 'stopped'
+                    }
+             }
+            context.set('state',state)
+//            node.warn(distribution) // debug
+            if (distribution === 'uniform' && min >= max) {
+                node.warn('must have max > min for uniform distribution')
+                return
             }
-            context.set('run',run);
-            if (run) {
-                node.status({fill:'green',shape:'dot'});
+            if (state === 'started') {
+                node.status({fill:'green',shape:'dot',text: 'started'})
+                loop()
             } else {
-                node.status({fill:'red',shape:'ring',text:'stopped'});
+                node.status(stoppedStatus)
             }
-            context.set('output',msg);
-            loop();
         })
         
         node.on('close',function(){
-            clearTimeout(startTimer);
-            node.status({fill:'red',shape:'ring',text:'stopped'});
+            clearTimeout(timer)
         })
         
         function loop() {
-            var delay = 0
-            msgToSend = context.get('output');
-            if (typeof msgToSend.topic !== 'undefined' && msgToSend.topic.toLowerCase() === node.controlTopic) {
-                msgToSend = null;
+            state = context.get('state')
+            if (state === 'running'){
+                node.send(RED.util.cloneMessage(msgToSend))
+            } else if  (state === 'started') {
+                msgToSend.count = -1
+                state = 'running'
+                context.set('state',state)
+            } else if (state === 'stopped') {
+                return
             }
-                node.send(msgToSend);
-            if (context.get('run')) {
-                var msgToSend = context.get('output');
+            if (state === 'running') { 
                 switch (distribution) {
                     case 'exponential':
-                        delay = expInterval(mean);
-                        break;
+                        delay = expInterval(expMean)
+                        break
                     case 'uniform':
-                        delay = uniformInterval(min,max);
-                        break;
+                        delay = uniformInterval(min,max)
+                        break
+                    case 'gaussian':
+                        delay = gaussianInterval(gaussMean,stdDev)
+                        break
                     default:
-                        node.error('No distribution specified');
+                        node.error('No distribution specified')
                         return
                 }
-                msgToSend.payload = node.outputPayload;
-                
-                if (msgToSend.payload === 'timestamp') {
-                    msgToSend.payload = Date.parse (new Date());
-
+                msgToSend.payload = payload
+                if (outputPayloadType === 'date') {
+                    msgToSend.payload = (new Date()).getTime()
                 }
-                msgToSend.topic = node.outputTopic;
-                msgToSend.delay = delay;
-                context.set('output',msgToSend);
-                startTimer = setTimeout(loop,1000 * delay);
-                node.status({fill:'green',text:delay.toFixed(2)});
+                msgToSend.topic = outputTopic
+                msgToSend.delay = delay
+                msgToSend.count ++
+                if (msgToSend.count >= maxEventCount) {
+                    state = 'stopped'
+                    context.set('state',state)
+                    node.status(stoppedStatus)   // debug
+                    return
+                }
+                timer = setTimeout(loop,1000 * delay)
+                node.status({fill:'green',shape:'dot',text:delay.toFixed(2)})
             }
         }
         
         function expInterval(m) { // see Knuth, Vol. 2, p. 133
-            var u = Math.random();
-            if (u === 0){
-                u = 5e-234;
-            }
-            return - m * Math.log(u);
+            let u = Math.random()
+            if (u === 0) {u = 5e-234}
+            return - m * Math.log(u)
         }
 
         function uniformInterval(min,max) {
-            return Math.random() * (max - min) + min;
+            return Math.random() * (max - min) + min
+        }
+        
+        function gaussianInterval(mu,sigma) { // Box–Muller method, see Knuth, Vol. 2, p. 122
+            let two_pi = 2 * Math.PI
+            let r, z0, z1, u1, u2
+            do {
+                u1 = Math.random()
+                u2 = Math.random()
+                r = sigma * Math.sqrt(-2.0 * Math.log(u1))
+                z0  = r * Math.cos(two_pi * u2) + mu
+                z1  = r * Math.sin(two_pi * u2) + mu
+            }
+            while (z0 <= 0)
+            return z0
         }
     }
     RED.nodes.registerType("event",RandomEventNode);
